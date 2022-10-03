4c9c73e79eb5182af9b7379c39237b3e9d3dcf07
Sito8943 <sito8943@gmail.com>
2022-10-02 21:52:00 -0400

    back button on docs

--- src/views/SitoDocs.jsx
+++ src/views/SitoDocs.jsx
@@ -3,12 +3,13 @@ import SitoContainer from "sito-container";
 
 // @mui/icons-material
 import Launch from "@mui/icons-material/Launch";
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 
 // @emotion/css
 import { css } from "@emotion/css";
 
 // @nextui-org
-import { Text, Link } from "@nextui-org/react";
+import { Button, Text, Link } from "@nextui-org/react";
 
 // own components
 import InViewComponent from "../components/InViewComponent/InViewComponent";
@@ -17,48 +18,86 @@ import DocCard from "../components/DocCard/DocCard";
 // contexts
 import { useLanguage } from "../contexts/LanguageProvider";
 
+// utils
+import { parseDelay } from "../utils/functions";
+
 const SitoDocs = () => {
   const { languageState } = useLanguage();
 
   return (
     <SitoContainer flexDirection="column" sx={{ padding: "24px" }}>
-      <Text h1>{languageState.texts.Docs.Title}</Text>
-      {languageState.texts.Docs.Components.map((item) => (
-        <InViewComponent key={item.id}>
-          <DocCard>
-            <SitoContainer alignItems="center" sx={{ marginBottom: "10px" }}>
-              <Link
-                color="primary"
-                href={item.link}
-                target="_blank"
-                rel="noopener"
+      <SitoContainer alignItems="center">
+        <Link href="/">
+          <Button
+            color="primary"
+            light
+            css={{
+              minWidth: "0px !important",
+              width: "40px",
+              transition: "all 500ms ease",
+              "&:hover": {
+                transform: "translateY(-5px)",
+              },
+              marginRight: "20px",
+              marginLeft: "5px",
+            }}
+          >
+            <ArrowCircleLeftIcon sx={{ fontSize: "40px" }} />
+          </Button>
+        </Link>
+        <Text h1 css={{ marginBottom: 5 }}>
+          {languageState.texts.Docs.Title}
+        </Text>
+      </SitoContainer>
+      <SitoContainer>
+        {languageState.texts.Docs.Components.map((item, i) => (
+          <InViewComponent key={item.id} delay={`${parseDelay(i, 0.3)}s`}>
+            <DocCard>
+              <SitoContainer
+                alignItems="center"
+                justifyContent="space-between"
+                sx={{ marginBottom: "10px" }}
               >
-                <Text h4 color="primary" css={{ marginBottom: 3 }}>
-                  {item.title}
-                </Text>
-                <Launch sx={{ marginLeft: "5px", fontSize: "20px" }} />
-              </Link>
-            </SitoContainer>
-            <hr className={css({ margin: "10px 0" })} />
-            <Text p>{item.description}</Text>
-            <hr className={css({ margin: "10px 0" })} />
-            <Text h5 css={{ margin: 0 }}>
-              {languageState.texts.Docs.Dependencies}
-            </Text>
-            <SitoContainer sx={{ flexWrap: "wrap" }}>
-              {item.dependencies.map((jtem) => (
                 <Link
-                  css={{ marginRight: "10px" }}
-                  key={jtem.name}
-                  href={jtem.link}
+                  color="primary"
+                  href={item.link}
+                  target="_blank"
+                  rel="noopener"
                 >
-                  {jtem.name} v: {jtem.version}
+                  <Text h4 color="primary" css={{ marginBottom: 3 }}>
+                    {item.title}
+                  </Text>
+                  <Launch sx={{ marginLeft: "5px", fontSize: "20px" }} />
                 </Link>
-              ))}
-            </SitoContainer>
-          </DocCard>
-        </InViewComponent>
-      ))}
+                <Text span size="$xs">
+                  {item.size}
+                </Text>
+              </SitoContainer>
+              <Text span size="$xs">
+                @{item.version}
+              </Text>
+              <hr className={css({ margin: "10px 0" })} />
+              <Text p>{item.description}</Text>
+              <hr className={css({ margin: "10px 0" })} />
+              <Text h5 css={{ margin: 0 }}>
+                {languageState.texts.Docs.Dependencies}
+              </Text>
+              <SitoContainer sx={{ flexWrap: "wrap" }}>
+                {item.dependencies.map((jtem) => (
+                  <Link
+                    css={{ marginRight: "10px" }}
+                    key={jtem.name}
+                    href={jtem.link}
+                  >
+                    {jtem.name}
+                    {jtem.version}
+                  </Link>
+                ))}
+              </SitoContainer>
+            </DocCard>
+          </InViewComponent>
+        ))}
+      </SitoContainer>
     </SitoContainer>
   );
 };
