7043bc698f1ff21edb83cd69dc64cdd0e40c3ed5
Sito8943 <sito@gmail.com>
2022-10-09 08:57:20 -0400

    Doing offcanvas

--- src/components/OffCanvas/OffCanvas.jsx
+++ src/components/OffCanvas/OffCanvas.jsx
@@ -20,12 +20,14 @@ const OffCanvas = (props) => {
   return (
     <SitoContainer
       sx={{
-        position: "fixed",
-        height: "100vh",
         top: 0,
         left: 0,
-        opacity: visible ? 1 : 0,
+        height: "100vh",
+        width: "100%",
+        position: "fixed",
+        transition: "all 500ms ease",
         zIndex: visible ? 99999 : -1,
+        background: "#222222ce",
       }}
     >
       <SitoContainer
@@ -34,15 +36,23 @@ const OffCanvas = (props) => {
           transition: "all 500ms ease",
           height: "100%",
           width: "300px",
-          transform: `translateX(${visible ? 0 : -300})`,
+          transform: `translateX(${visible ? 0 : "-300px"})`,
           background: "#333444",
         }}
       >
-        <SitoContainer justifyContent="flex-end" sx={{ width: "100%" }}>
+        <SitoContainer
+          sx={{ width: "100%", position: "relative", height: "40px" }}
+        >
           <Button
             flat
             rounded
-            css={{ margin: "10px", width: "40px", minWidth: "0" }}
+            css={{
+              margin: "10px",
+              width: "40px",
+              minWidth: "0",
+              position: "absolute",
+              right: "15px",
+            }}
             onPress={handleClose}
           >
             <ClearIcon />
@@ -53,7 +63,7 @@ const OffCanvas = (props) => {
           <ul className={`uk-nav uk-nav-primary uk-nav-left`}>
             {languageState.texts.Navbar.Links.map((item) => (
               <li key={item.label}>
-                <Button rounded flat color="primary">
+                <Button light rounded flat color="primary">
                   <Link href={item.to}>{item.label}</Link>
                 </Button>
               </li>
