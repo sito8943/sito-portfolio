029598f5058c7baab9c77f5c49510d1365d4cbee
Sito8943 <sito8943@gmail.com>
2023-01-02 11:35:41 -0500

    0.11.3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { getUserLanguage } from "some-javascript-utils/browser";
 
 // next-themes
 import { useTheme as useNextTheme } from "next-themes";
+
+// context
+import { useLanguage } from "./contexts/LanguageProvider";
 
 // views
 import Home from "./views/Home";
@@ -12,6 +16,15 @@ import NotFound from "./views/NotFound";
 
 const App = () => {
   const { setTheme } = useNextTheme();
+  const { setLanguageState } = useLanguage();
+
+  useEffect(() => {
+    try {
+      setLanguageState({ type: "set", lang: getUserLanguage() });
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
 
   useEffect(() => {
     setTheme("dark");
