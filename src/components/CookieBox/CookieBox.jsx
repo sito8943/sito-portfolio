1fbfa66826dc60d32474633eea62815cd5de66ff
Sito8943 <sito8943@gmail.com>
2023-04-14 07:57:46 -0700

    CookieBox

--- src/components/CookieBox/CookieBox.jsx
+++ src/components/CookieBox/CookieBox.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function CookieBox(props) {
+  const [accept, setAccept] = useState(false);
+
+  useEffect(() => {
+    if (accept) {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        // eslint-disable-next-line no-undef
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "G-D9HLQQ818J");
+    }
+  }, [accept]);
+
   return <div></div>;
 }
 
