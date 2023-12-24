81434f53bd765177b92b82c56f1382de097dfa79
Sito8943 <sito8943@gmail.com>
2023-12-24 18:35:13 -0500

    scroll on load

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,6 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { scrollTo } from "some-javascript-utils/browser";
 
 // own components
 import Loading from "../components/Loading/Loading";
@@ -15,6 +17,15 @@ import Contact from "../layouts/Contact/Contact";
 import Footer from "../components/Footer/Footer";
 
 const Home = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const { hash } = location;
+    setTimeout(() => {
+      scrollTo(document.getElementById(hash?.substring(1))?.offsetTop);
+    }, 500);
+  }, [location]);
+
   return (
     <Suspense fallback={<Loading />}>
       <ToTop />
