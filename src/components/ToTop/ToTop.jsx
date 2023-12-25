724203d030eb6baf5993427ce095a4865dfb1a7e
Sito8943 <sito8943@gmail.com>
2023-12-25 18:38:09 -0500

    using ToTop from @sito/ui

--- src/components/ToTop/ToTop.jsx
+++ src/components/ToTop/ToTop.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 
+// @emotion/css
 import { css } from "@emotion/css";
 
-import PropTypes from "prop-types";
+// @sito/ui
+import { ToTop as ScrollToTop } from "@sito/ui";
 
-// @fortawesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import PropTypes from "prop-types";
 
 // utils
 import { scrollTo } from "../../utils/functions";
@@ -15,28 +15,23 @@ import { scrollTo } from "../../utils/functions";
 const ToTop = () => {
   const { t } = useTranslation();
 
-  const [visible, setVisible] = useState(false);
   const [bottom, setBottom] = useState("15px");
   const [push, setPush] = useState(false);
 
-  const onScroll = useCallback(
-    (e) => {
-      const top = window.pageYOffset || document.documentElement.scrollTop;
-      setVisible(top > 100);
+  const onScroll = useCallback((e) => {
+    const top = window.pageYOffset || document.documentElement.scrollTop;
 
-      const fullHeight = Math.max(
-        document.body.scrollHeight,
-        document.documentElement.scrollHeight,
-        document.body.offsetHeight,
-        document.documentElement.offsetHeight,
-        document.body.clientHeight,
-        document.documentElement.clientHeight
-      );
-      if (fullHeight - top === window.innerHeight) setPush(true);
-      else setPush(false);
-    },
-    [setVisible]
-  );
+    const fullHeight = Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight,
+      document.body.clientHeight,
+      document.documentElement.clientHeight
+    );
+    if (fullHeight - top === window.innerHeight) setPush(true);
+    else setPush(false);
+  }, []);
 
   useEffect(() => {
     if (!push) setBottom("15px");
@@ -44,7 +39,6 @@ const ToTop = () => {
   }, [push]);
 
   useEffect(() => {
-    setVisible(top > 100);
     window.addEventListener("scroll", onScroll);
     return () => {
       window.removeEventListener("scroll", onScroll);
@@ -52,18 +46,19 @@ const ToTop = () => {
   }, [onScroll]);
 
   return (
-    <button
+    <ScrollToTop
       name="to-top"
       aria-label={t("_common:ariaLabels.toTop")}
       onClick={() => scrollTo(0)}
-      className={`${
-        visible ? "scale-[1]" : "scale-0 pointer-events-none"
-      } primary submit cta icon-button ${css({
+      color="primary"
+      shape="filled"
+      className={`${css({
         bottom,
-      })} z-50 rounded-full fixed right-3 mt-4 min-w-0 w-10 !transition-all hover:-translate-y-1`}
-    >
-      <FontAwesomeIcon icon={faArrowUp} />
-    </button>
+        svg: {
+          marginLeft: "-1px",
+        },
+      })} right-3`}
+    />
   );
 };
 
