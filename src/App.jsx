ceeae4c7e26d30d7334182277c19df1e08efee75
Sito8943 <sito8943@gmail.com>
2023-12-25 18:38:18 -0500

    suspense app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from "react";
+import React, { Suspense, useEffect } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import loadable from "@loadable/component";
+
+// @sito/ui
+import { SplashScreen } from "@sito/ui";
 
 // views
-import Home from "./views/Home";
-import NotFound from "./views/NotFound";
+const Home = loadable(() => import("./views/Home"));
+const NotFound = loadable(() => import("./views/NotFound"));
 
 const App = () => {
   return (
-    <BrowserRouter basename="sito-portfolio">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Suspense
+      fallback={
+        <SplashScreen
+          visible
+          logo={
+            <div>
+              <h1 className="font-bold text-4xl text-plight logo">
+                {"<Sito />"}
+              </h1>
+            </div>
+          }
+        />
+      }
+    >
+      <BrowserRouter basename="sito-portfolio">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
