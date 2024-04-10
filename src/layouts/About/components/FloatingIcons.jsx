d99d2cae14b931fcca7261b2472a8ff5225a47e4
Sito8943 <sito8943@gmail.com>
2024-04-09 22:15:21 -0400

    about floawting

--- src/layouts/About/components/FloatingIcons.jsx
+++ src/layouts/About/components/FloatingIcons.jsx
@@ -3,21 +3,11 @@ import React, { Fragment, memo } from "react";
 // components
 import MiniLogo from "../../../components/Logo/MiniLogo";
 
-// @emotion/css
-import { css } from "@emotion/css";
-
 const FloatingIcons = () => {
   return (
     <Fragment>
-      <div
-        className={`sm:hidden scale-animation ${css({
-          width: "100px",
-          height: "100px",
-          right: "5%",
-          top: "15%",
-        })}`}
-      >
-        <MiniLogo className={`float-y opacity-50 h-[6rem]`} />
+      <div className="sm:hidden scale-animation big-floating">
+        <MiniLogo className="float-y opacity-50 h-[6rem]" />
       </div>
     </Fragment>
   );
