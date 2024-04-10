e18ac0207e02c67716c9ba13648bef46444ea4bc
Sito8943 <sito8943@gmail.com>
2024-04-09 22:14:46 -0400

    phone floating

--- src/layouts/Contact/components/FloatingIcons.jsx
+++ src/layouts/Contact/components/FloatingIcons.jsx
@@ -4,20 +4,10 @@ import React, { Fragment, memo } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
-// @emotion/css
-import { css } from "@emotion/css";
-
 const FloatingIcons = () => {
   return (
     <Fragment>
-      <div
-        className={`sm:hidden scale-animation ${css({
-          width: "100px",
-          height: "100px",
-          right: "5%",
-          top: "15%",
-        })}`}
-      >
+      <div className="sm:hidden scale-animation big-floating">
         <FontAwesomeIcon
           icon={faPhone}
           className="float-y text-green opacity-50 text-[6rem]"
