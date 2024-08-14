fb87b7f597d7b26cc375ce9c0266ff81faf7332c
Sito8943 <sito8943@gmail.com>
2024-08-13 23:16:36 -0400

    added better styles for about cards

--- src/layouts/About/components/Card.jsx
+++ src/layouts/About/components/Card.jsx
@@ -14,7 +14,7 @@ function Card({ image, alt, onClick, text, more, noHover }) {
       aria-label={t("_common:ariaLabels.clickToDialog")}
       className={`${
         noHover
-          ? "border-0"
+          ? "!border-0"
           : "hover:-translate-y-2 hover:shadow-lg hover:shadow-yellow/40 transition duration-300 border-placeholder-dark"
       } bg-dark-background2 md:w-full md:h-auto w-[300px] h-[250px] rounded-xl border-[1px] `}
     >
@@ -23,9 +23,9 @@ function Card({ image, alt, onClick, text, more, noHover }) {
         alt={alt}
         className="w-full md:h-[300px] h-[150px] rounded-t-xl object-cover object-bottom"
       />
-      <div className="p-5">
+      <div className="p-1 px-5">
         <p className="text-lg font-bold text-center">{text}</p>
-        <p className="text-center">{more}</p>
+        <p className="text-center mb-5">{more}</p>
       </div>
     </article>
   );
